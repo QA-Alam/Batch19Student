99b5cbef3804a74cda508ef1fb317d134a13611e
mohammedalam <mohammedalam@mohammeds-mbp.cable.rcn.com>
2021-09-20 17:12:50 -0400

    alam

--- Batch19Students/Fetatures/ScenarioOutLine.feature
+++ Batch19Students/Fetatures/ScenarioOutLine.feature
@@ -6,8 +6,8 @@ Scenario Outline: Successful Login with Valid Credentials scenario Outline
 	Given User able to open chrome browser 
 	And  Put "<URL>" and go to login page 
 	When User able to click my account 
-	And User able to use valid username"<userName>" 
-	And User able to put valid  password"<password>" 
+	And User able to use valid username "<userName>" 
+	And User able to put valid  password "<password>" 
 	And User able to click singin button 
 	Then User able to validated Login status 
 	And closeing the browser 
--- Batch19Students/src/main/java/com/utility/CommonUtility.java
+++ Batch19Students/src/main/java/com/utility/CommonUtility.java
@@ -1,5 +1,28 @@
 package com.utility;
 
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class CommonUtility {
 
+	static WebDriver driver;
+
+	public boolean isElementPresent(WebElement element, WebDriver driver, long timeout) {
+		try {
+			WebDriverWait wait = new WebDriverWait(driver, timeout);
+			element = wait.until(ExpectedConditions.visibilityOf(element));
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+	public static void drawBorder(WebElement webelement, String color) {
+		WebElement element_node = webelement;
+		JavascriptExecutor jse = (JavascriptExecutor) driver;
+		jse.executeScript("arguments[0].style.border='3px solid " + color + "'", element_node);
+
+	}
 }
